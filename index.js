98f06c0d2fa94798f3329e585546df7c5da25483
Matthew Tan <tanmatt804@gmail.com>
2021-11-15 14:57:07 +0000

    finished

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //write your code here
+function findLongestWord(arr) {
+    let longest = "";
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (count < arr[i].length) {
+            count = arr[i].length;
+            longest = arr[i];
+        }
+    }
+    
+    return longest;
+}
+
+console.log(findLongestWord(["The","quick","brown", "fox", "jumped", "over", "the", "lazy", "dog"])); //returns "jumped"
+console.log(findLongestWord(["jazzy", "jumpy", "quaky"])); //returns "jazzy"
